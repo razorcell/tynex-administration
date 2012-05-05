f8f980ca3c302027d5c15096a931f0a98472ad86
khalifa <khalifa.rmili@gmail.com>
2012-05-05 18:13:09 +0100

    home page backup somme little pblm

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,24 +1,111 @@
 <?php
 
 class IndexController extends Zend_Controller_Action {
-	
+	private $config = NULL;
+	private $db = NULL;
+	private $writer = NULL;
+	private $logger = NULL;
 	public function init() {
+		$this->ctrl = $this->_request->getControllerName ();
+		$this->view->ctrl = $this->ctrl;
+		$this->writer = new Zend_Log_Writer_Stream ( APPLICATION_PATH . '/../tests/logs' );
+		$this->logger = new Zend_Log ( $this->writer );
+		
+		$this->config = new Zend_Config_Ini ( APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV );
+		
+		try {
+			$this->db = Zend_Db::factory ( $this->config->database );
+			$this->db->getConnection ();
+			$this->db->getProfiler ()->setEnabled ( true );
+		} catch ( Zend_Db_Adapter_Exception $e ) {
+			echo $e->getMessage ();
+		} catch ( Zend_Exception $e ) {
+			echo $e->getMessage ();
+		}
 		$this->view->general_icon = 'ico color home';
-		$this->view->title = 'Adminisatration de Tynex Media';
-		/*
-		 * //connect to the database try {$db =
-		 * Zend_db::factory('Pdo_Mysql',array( 'host' => '127.0.0.1', 'username'
-		 * => 'root', 'password' => '', 'dbname' => 'tynexADMIN' )); }catch
-		 * (Zend_Db_Adapter_Exception $e) { echo $e->getMessage();
-		 * }catch(Zend_Exception $e) { echo $e->getMessage(); } $sql = 'SELECT *
-		 * FROM clients'; $this->view->list_clients = $db->fetchAssoc($sql);
-		 */
+		$this->view->title = 'Administration de Tynex Media';
 	}
-	
 	public function indexAction() {
-		// action body
-		//$this->view->message = 'The service N° 12 is going to expire in 14 days';
+		$this->db->setFetchMode ( Zend_Db::FETCH_OBJ );
+		// service actif
+		$sql = 'SELECT count( id_service ) AS totale
+					FROM service
+					WHERE STATUS = \'Actif\'';
+		$this->view->services_actifs = $this->db->fetchRow ( $sql );
+		// service interrompu
+		
+		$sql = 'SELECT count( id_service ) AS totale
+		FROM service
+		WHERE STATUS = \'Interrompu\'';
+		$this->view->services_interrompus = $this->db->fetchRow ( $sql );
+		// projets actif
+		$sql = 'SELECT count( id_projet ) AS totale
+		FROM projet
+		WHERE STATUS = \'Actif\'';
+		$this->view->projets_actifs = $this->db->fetchRow ( $sql );
+		// projets interrompu
+		
+		$sql = 'SELECT count( id_projet ) AS totale
+		FROM projet
+		WHERE STATUS = \'Interrompu\'';
+		$this->view->projets_interrompus = $this->db->fetchRow ( $sql );
+		
+		
+		//service non payes
+		$sql = 'SELECT count( id_service ) AS totale
+		FROM service
+		WHERE paye = \'Non\'';
+		$this->view->services_non_paye = $this->db->fetchRow ( $sql );
+		//projet non payes
+		$sql = 'SELECT count( id_projet ) AS totale
+		FROM projet
+		WHERE paye = \'Non\'';
+		$this->view->projets_non_paye = $this->db->fetchRow ( $sql );
+		
+		$projet_en_cour = 0;
+		$projet_fini = 0;
+		$sql = 'SELECT *
+		FROM projet';
+		$list_projets = $this->db->fetchAssoc ( $sql );
+		foreach ( $list_projets as $projet ) {
+			if ($projet ['progression'] == 100) {
+				$projet_fini ++;
+			} else {
+				$projet_en_cour ++;
+			}
+		
+		}
+		$this->view->projets_fini = $projet_fini;
+		$this->view->projets_en_cour = $projet_en_cour;
+		//total employes
+		$sql = 'SELECT count( id_employe ) AS totale FROM employe';
+		$this->view->employes = $this->db->fetchRow ( $sql );
+		
+		$sql = 'SELECT date_debut
+		FROM service WHERE id_service = 4';
+		//Traitement d'expiration
+		//current_date
+		$services_expired = array();
+		$current_date = Zend_Date::now();
+		//get the services that will expire in 1 month
+		$sql = 'SELECT * FROM service';
+		$list_services = $this->db->fetchAssoc ( $sql );
+		foreach($list_services as $service){
+			$date_fin_string = $service['date_fin'];
+			$date_fin = new Zend_Date($date_fin_string);
+			$diff = $date_fin->sub($current_date)->toValue();
+			$months = floor(((($diff/60)/60)/24)/30);
+			if($months <= 1){
+				array_push($services_expired, array('id_service' => $service['id_service'],
+																		'diff' => $diff));
+			}
+		}
+		$this->view->services_expired = $services_expired;
+		//send list of services and type_service to view
+		$sql = 'SELECT *
+		FROM service NATURAL JOIN type_service';
+		$this->view->services__types = $this->db->fetchRow ( $sql );
+		
+		
 	}
-
 }
-
